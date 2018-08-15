88cc7e7902faf496557fff775e901f71e10c8007
iNeelPatel <neelpatel.6531@icloud.com>
2018-08-15 23:29:35 +0530

    add server.js with hello world

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get("/", (req, res) => res.send("Hello..!"));
 
 const port = process.env.PORT || 5000;
 
